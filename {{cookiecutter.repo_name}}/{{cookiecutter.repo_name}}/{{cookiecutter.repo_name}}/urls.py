889cc4d85b8f7222ec61d50d16b6e2c63c3a175b
Ben Warren <bwarren2@gmail.com>
2015-07-24 21:41:28 -0400

    No need for extra urls if debug

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/urls.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/urls.py
@@ -1,6 +1,4 @@
-from django.conf import settings
 from django.conf.urls import patterns, include, url
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.views.generic import TemplateView
 from .views import BootstrapView
@@ -14,4 +12,4 @@ urlpatterns = patterns(
     url(r'^admin/', include(admin.site.urls)),
     url(r'^styles/$', BootstrapView.as_view(), name='bootstrap_test'),
 
-) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+)
